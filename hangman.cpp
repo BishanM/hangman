f04c1526a6e1cf1fa87a6cacf429e87db8ccbfca
Austen L. Strine <austenstrine@gmail.com>
2017-03-16 12:43:54 -0600

    More conventions correction

--- hangman.cpp
+++ hangman.cpp
@@ -1,5 +1,5 @@
 //Challenge: Create a hangman puzzle program
-//Player gets 175% as many guesses as there are letters in the word.
+//Player gets 150% as many guesses as there are letters in the word.
 
 #include <iostream>
 #include <fstream>
@@ -28,24 +28,34 @@ int main()
 	ifstream inWords;
 	inWords.open("corncob_caps.txt", ios::in);
 	vector<string> wordVector;
-		if (inWords.is_open())								//make sure input file opened properly
-		{	
-			while(!inWords.eof())					//put every word listed in the file into an array to make program speedy
-				{	
+		if (inWords.is_open())
+		{	//make sure input file 
+			//opened properly
+			while(!inWords.eof())					
+				{	//put every word listed in the file 
+					//into an array to make program speedy	
 					string word = "";
 					getline(inWords, word);
 					wordVector.push_back(word);
-				}//end for
+				}//end while
 
 			int scoreBoard[2][2] = {	{0,0},
 										{0,0} };
 
-				while (true) 									//make the game run until a sentinel value calls break;
-				{	
+				while (true)
+				{	//make the game run until a sentinel 
+					//value calls break;
 					int randNum = randomInteger();
 					
 					
-						#ifdef WIN32  //unix for some reason compiles a whitespace character at the end of this string
+						#ifdef WIN32  
+							// unix/mac for some reason compiles a whitespace 
+							// character(?) at the end of this string. I think
+							// it has to do with the C way of making a string
+							// as an array of char's with the last char being
+							// '\n' - pure speculation. It doesn't appear when
+							// the string is displayed, but it counts as part
+							// of the length when using answer.length();
 							string answer = wordVector.at(randNum);
 						#else 
 							string answer = wordVector.at(randNum);
@@ -81,10 +91,13 @@ int main()
 									guess = ""; 
 								 	getline(cin, guess);
 									transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
-									char onlyLetters = 'Y'; //set to Y - if unaltered, indicates that menu-call has not ocurred
+									char onlyLetters = 'Y'; 
+									//set to Y - if unaltered, 
+									//indicates that menu-call 
+									//has not ocurred
 									
-										if (guess == "!END")//checks for sentinel value
-										{	
+										if (guess == "!END")
+										{	//checks for sentinel value
 											inWords.close();
 											return 0;
 										}//end returnIF
@@ -93,8 +106,13 @@ int main()
 										{	
 											menuCoutBlock(answerClue);
 																						
-											guess = "";//ensures it will ask for another guess without registering '!' as the guess
-											onlyLetters = 'M';//prevents onlyLetters error from triggering, 'M' for menu
+											guess = "";
+											// ensures it will ask for 
+											// another guess without registering 
+											// '!' as the guess
+											onlyLetters = 'M';
+											// prevents onlyLetters error from 
+											// triggering, 'M' for menu
 										}//end menuIF
 										
 										if(guess == "!WORD")
@@ -104,7 +122,7 @@ int main()
 											<< endl;
 											onlyLetters = ' ';
 												while (onlyLetters != 'Y')
-												{	
+												{	//runs until the input is passed as valid
 													getline(cin, guess);
 													transform(guess.begin(), guess.end(), guess.begin(), ::toupper);
 													
@@ -113,9 +131,24 @@ int main()
 															inWords.close();
 															return 0;
 														}// end returnIF
-
-													checkForOnlyLetters(guess, onlyLetters)	;
-																											
+													
+													//getValidInputForGuess();
+													//{
+														
+														checkForOnlyLetters(guess, onlyLetters)	;
+														// This will set onlyLetters to 'N' if it 
+														// finds any non-letters									
+														
+														//checkForProperGuessLength();
+														// This will set onlyLetters to 'L' if guess
+														// is too long, and if it's already set to 'L',
+														// it will trigger a loss. Does the same if the
+														// guess is too short, sets to 'S'. Otherwise, 
+														// sets onlyLetters to 'Y' if it has not been set
+														// to 'N' above. Hmm... Should probably switch 
+														// the position of these for simplification.
+														//{
+													
 														if (guess.length() > answer.length())
 														{	
 															cout
@@ -126,7 +159,7 @@ int main()
 																	cout
 																	<< endl << "You took the long route again! Now Ascii's neck will be longer..."
 																	<< endl;
-																	break;
+																	break;//breaks out of y_onlyLettersWHILE
 																}
 																else
 																{	
@@ -144,17 +177,22 @@ int main()
 																	cout
 																	<< endl << "Not again! Now Ascii will have a short drop and a sudden stop..."
 																	<< endl;
-																	break;
+																	break;//breaks out of y_onlyLettersWHILE
 																}
 																else
 																{
-																onlyLetters = 'H';
+																	onlyLetters = 'H';
 																}//end inner h_onlyLettersIF
 														}
 														else if (onlyLetters != 'N')
 														{	
 															onlyLetters = 'Y';
-														}//end all_onlyLettersIF									
+														}//end all_onlyLettersIF
+													
+														//} //end checkForProperGuessLength();
+													
+													//} //end getValidInputForGuess();
+													
 												}//end y_onlyLettersWHILE
 											
 											//cout << guess << endl;
@@ -163,22 +201,28 @@ int main()
 													cout
 													<< endl << "Oh no! The correct answer is " + answer + "! You lose!"
 													<< endl << endl;
+													
 													fullGuess = 'N';
-													++scoreBoard[1][1];		//losses by full guess
-												}
+													++scoreBoard[1][1];
+													//losses by full guess
+												}//if
 												else
 												{	
 													cout
 													<< endl << "That's right! You win! The answer is " + answer + "!"
 													<< endl << endl;
+													
 													fullGuess = 'Y';
-													++scoreBoard[0][1];		//wins by full guess
+													++scoreBoard[0][1];
+													//wins by full guess
 												}//end inner if
-											onlyLetters = 'W';//prevents onlyLetters error from triggering, 'W' for word guess
-										}
+											onlyLetters = 'W';
+											// prevents onlyLetters error from 
+											// triggering, 'W' for word guess
+										}//end !wordIF
 										
-										if (guess == "!SCORE")//checks for score call
-										{	
+										if (guess == "!SCORE")
+										{	//checks for score call
 											cout 
 											<< endl
 											<< "You've won " << scoreBoard[0][0] << " times by letter guess."
@@ -190,13 +234,17 @@ int main()
 											<< "You've lost " << scoreBoard[1][1] << " times by full word guess."
 											<< endl << endl;
 											
-											guess = "";//ensures it will ask for another guess without registering '!' as the guess
-											onlyLetters = 'S';//ensures an onlyLetters error isn't triggered, 'S' for score exception
-										}
+											guess = "";
+											// ensures it will ask for another guess 
+											// without registering '!' as the guess
+											onlyLetters = 'S';
+											// ensures an onlyLetters error isn't 
+											// triggered, 'S' for score exception
+										}//end !scoreIF
 										
 										if (onlyLetters == 'Y')
 										{	
-											checkForOnlyLetters(guess, onlyLetters);
+											checkForOnlyLetters(guess, onlyLetters); //redundancy here needs to be corrected///////////////
 											
 											if (onlyLetters == 'N') //if non-letters are detected, triggers error
 											{	
@@ -204,20 +252,23 @@ int main()
 												<< endl << "Only letters are allowed!"
 												<< endl;
 												guess = "";
-											}
+											}//if
 											else 
 											{	
-													for (int pong = answer.length(); pong > -1; pong --) //replace each dash with a correct letter
+													for (int pong = answer.length(); pong > -1; pong --) 
 													{		
 															if (answer.substr(pong, 1) == guess.substr(0,1))
-															{	
+															{	// if the letter is correct, replace the 
+																// corresponding dash in the hint with it.
 																answerClue.replace(pong, 1, guess.substr(0,1));
 																goodGuess = 'Y';
 															}//end if
 													}//end pongFor
+												
 											}//end n_onlyLettersIF
 											
 										}// end y_onlyLettersIF
+									
 								}//end invalidGuessWHILE	
 							
 							
@@ -227,7 +278,8 @@ int main()
 									break;//breaks out of numOfGuessesFOR
 								}
 								else if (fullGuess == 'N')
-								{
+								{	// displays Ascii's untimely demise
+									// if you guessed the full word wrong
 									cout	<< "                                          "
 									<< endl << "                  -----                   "
 									<< endl << "                  |  \\|                   "
@@ -273,7 +325,7 @@ int main()
 									<< endl << answerClue
 									<< endl << "You have " << numOfGuesses-1 << " guesses left."
 									<< endl;
-								}//end winLoseIF
+								}//end winLoseContinueIF
 								
 								
 								
@@ -341,9 +393,12 @@ int main()
 									<< endl << "___________/    |( )        \\_____________"
 									<< endl << "          Ascii has been Hung!            "
 									<< endl << endl;
-								}
+								}//end asciiGraphicIF
+							
 						}//end outer numOfGuessesFor
+					
 				}//end trueWhile
+			
 		}//if
 		else
 		{	
@@ -366,6 +421,7 @@ void checkForOnlyLetters(string g, char &oL)
 				<< endl;
 				break;
 			}//end n_onlyLettersIF
+		
 	}//end pingFor
 
 }
@@ -392,11 +448,9 @@ void findDeleteLetters(string g, string &abc)
 		while (abc.find(ch, 0) != std::string::npos)
 		{	
 			abc.erase(abc.find(ch, 0), 1);
-		}
+		}//end abcEraseWHILE
 	cout 
 	<< endl << "Available letters: " << abc;
-	//http://www.cplusplus.com/reference/string/string/rfind/
-	//delete any g found in abc, cout abc
 }
 
 void bubbleSortLetters(string &lets)
@@ -415,7 +469,9 @@ void bubbleSortLetters(string &lets)
 					lets.replace(i-1, 1, plH, 0, 1);
 					sntnl = 'N';
 				}//end if
+				
 			}//end for
+			
 		}//end while
 	cout
 	<< endl << "     Used letters: " << lets
